8134c63bba1524cb57cd0177b8b23d0ed6a32535
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-08-01 21:44:08 -0300

    Adding the Header, the Cart and the Meals component

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import meal from '../../../assets/meal.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+    return (
+        <>
+            <header className={classes.header}> 
+                <h1>ReactMeals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={['main-image']}>
+                <img src={meal} alt='A Table full of delicious food'/>
+            </div>
+        </>
+    )
+}
+
+export default Header

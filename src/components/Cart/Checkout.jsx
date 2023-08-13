aabdb1f11aec7a04d3cd1bd4005612b1efce887e
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-08-13 10:31:53 -0300

    Finishing the Food App

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -44,7 +44,12 @@ const Checkout = (props) => {
         return;
     }
 
-
+        props.onConfirm({
+            name: enteredName,
+            street: enteredStreet,
+            city: enteredCity,
+            postalCode: enteredPostal
+    });
 };
 
     const nameControlClasses = `${classes.control} ${formInputsValidity.name ? '' : classes.invalid}`;
